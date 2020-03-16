9311de9f9a90d6d7f8d1ab9ec603bb8e7782ee89
Lauren Hallau <lhallau@gmail.com>
2020-03-16 16:46:43 -0500

    directory structure complete

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+//Dependencies
 const express = require("express");
-const handlebars = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const mysql = require("mysql");
+
+//Create express app
+var app = express();
+
+//Set the port of the application- ready for Heroku
+var PORT = process.env.PORT || 8080;
+
+//Static directory for css/js/images
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
