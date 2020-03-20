f11e6503c81d6880449d963426f7b4b21d005540
Lauren Hallau <lhallau@gmail.com>
2020-03-20 18:55:43 -0500

    heroku ready

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,26 @@
 //Connecting Node to MySQL
 const mysql = require("mysql");
+var connection;
+//getting app ready for heroku
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+};
 
-//Creating connection to MySQL
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+// //Creating connection to MySQL
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "burgers_db"
+// });
 
 connection.connect(function (err) {
     if (err) {
